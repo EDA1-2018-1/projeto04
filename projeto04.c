1065937717d0f30fef9c82105ac1c58522e4767b
Max <maxhb.df@gmail.com>
2018-06-03 16:11:10 -0300

    inicio

---
+++ projeto04.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct lista {
+  float info;
+  struct lista* prox;
+} Lista;
+
+typedef struct fila {
+  Lista* ini;
+  Lista* fim;
+} Fila;
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
