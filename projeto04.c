36db24bfff7b0f9e4d6efe94de771021d14d806f
Max <maxhb.df@gmail.com>
2018-06-03 17:10:18 -0300

    funções referentes à fila, ainda resta pensar na lógica do problema

--- projeto04.c
+++ projeto04.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define UnTempo 5
 
+//-----------------------------------------------------
+/* Declarção de Funções*/
+Fila* cria_Fila();
+int insere_Fila(Fila* fi, Lista li);
+int remove_Fila(Fila* fi);
+void libera_Fila(fi);
+
+//-----------------------------------------------------
 typedef struct lista {
   float info;
   struct lista* prox;
@@ -13,6 +22,63 @@ typedef struct fila {
 } Fila;
 
 int main(int argc, char const *argv[]) {
-  /* code */
+
+  //printf("Aeroporto Internacional de Brasília\n", );
+
   return 0;
+}
+
+//-----------------------------------------------------
+/*Funções*/
+
+Fila* cria_Fila(){
+  Fila* fi = (Fila*) malloc(sizeof(Fila));
+  if(fi != NULL){
+    fi->fim = NULL;
+    fi->ini = NULL;
+  }
+  return fi;
+}
+//-------------------------------------------------------
+void libera_Fila(Fila* fi){ // DELETAR A FILA (NÃO SEI SE É NECESSÁRIO)
+  if (fi != NULL){
+    Lista* no;
+    while (fi->ini != NULL) {
+      no = fi->ini;
+      fi->ini = fi->ini->prox;
+      free(fi);
+    }
+  }
+}
+//-------------------------------------------------------
+int insere_Fila(Fila* fi, Lista li){ //Função para inserir no fim
+  if(fi --NULL) return 0;
+  Lista* li = (li*) malloc(sizeof(Lista));
+
+  if(li == NULL) return 0;
+  li->info = li;
+  li->prox = NULL;
+
+  if(fi->fim == NULL){
+    fi->ini = li;
+  }
+  else{
+    fi->fim->prox = li;
+  }
+  return 1;
+}
+//--------------------------------------------------------
+int remove_Fila(Fila* fi){
+  if(fi == NULL) return 0;
+  if(fi->ini == NULL){
+    return 0;
+  }
+  Lista *li = fi->inicio;
+  fi->ini = fi->ini->prox;
+
+  if(fi->ini == NULL){
+    fi->fim = NULL;
+  }
+  free(li);
+  return 1;
 }
